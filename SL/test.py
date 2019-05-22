7c3b7889573af05aa233b407d04f93e6e3311483
fstovarr <fstovarr@unal.edu.co>
2019-05-22 12:43:50 -0500

    Looks good X100000

--- SL/test.py
+++ SL/test.py
@@ -4,7 +4,7 @@ import Parser.parser as parser
 from codeReader import fullCodeReader, SyntaxException, LexicException
 
 localPath = Path(__file__).parent
-sl_path = localPath / 'Parser/1.txt'
+sl_path = localPath / 'Parser/5.txt'
 lexerOutPath = localPath / 'Lexer/data/out.txt'
 
 try:
